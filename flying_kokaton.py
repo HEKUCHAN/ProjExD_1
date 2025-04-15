6346b13aca2aa08847c502d5951ff5e436df869f
HEKUCHAN <heitorhirose@gmail.com>
2025-04-15 16:26:23 +0900

    final_task

--- flying_kokaton.py
+++ flying_kokaton.py
@@ -9,14 +9,23 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 def main():
     pg.display.set_caption("はばたけ！こうかとん")
     screen = pg.display.set_mode((800, 600))
+    screen_rect = screen.get_rect()
+
     clock = pg.time.Clock()
+
     bg_img = pg.image.load("fig/pg_bg.jpg")
     fliped_bg_img = pg.transform.flip(bg_img, True, False)
+
     kokaton_img = pg.transform.scale(
         pg.transform.flip(pg.image.load("fig/3.png"), True, False), (60, 60)
     )
     koukaton_rect = kokaton_img.get_rect()
-    koukaton_rect.center = 300, 200
+
+    # 中央にこうかとんを配置
+    koukaton_rect.center = screen_rect.center
+
+    # こうかとんのすすむスピード
+    kokaton_speed = 1
 
     tmr = 0
     while True:
@@ -33,29 +42,22 @@ def main():
         screen.blit(kokaton_img, koukaton_rect)
 
         key_lst = pg.key.get_pressed()
-        if key_lst[pg.K_UP]:
-            if koukaton_rect.top > 0:
-                koukaton_rect.move_ip(0, -1)
-            else:
-                koukaton_rect.top = 0
-        if key_lst[pg.K_DOWN]:
-            if koukaton_rect.bottom < screen.get_height():
-                koukaton_rect.move_ip(0, 1)
-            else:
-                koukaton_rect.bottom = screen.get_height()
-        if key_lst[pg.K_LEFT]:
-            if koukaton_rect.left > 0:
-                koukaton_rect.move_ip(-1, 0)
-            else:
-                koukaton_rect.left = 0
+        up, down = key_lst[pg.K_UP], key_lst[pg.K_DOWN]
+        koukaton_rect.move_ip(0, down - up)
+
+        # こうかとんが画面からはみ出ないように制限
+        if koukaton_rect.top < screen_rect.top:
+            koukaton_rect.top = screen_rect.top
+        if koukaton_rect.bottom > screen_rect.bottom:
+            koukaton_rect.bottom = screen_rect.bottom
+
         if key_lst[pg.K_RIGHT]:
-            if koukaton_rect.right < screen.get_width():
-                koukaton_rect.move_ip(1, 0)
-            else:
-                koukaton_rect.right = screen.get_width()
+            kokaton_speed = 3
+        else:
+            kokaton_speed = 1
 
         pg.display.update()
-        tmr += 1
+        tmr += kokaton_speed
         clock.tick(200)
 
 
